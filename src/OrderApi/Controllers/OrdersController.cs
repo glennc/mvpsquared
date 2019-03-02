4f34f4bb6094f7ae89d6134da871f08e849dae19
Glenn Condron <glennc@microsoft.com>
2019-03-01 16:27:38 -0800

    orders can be submitted.

---
+++ src/OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using OrderApi.Models;
+
+namespace OrderApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private ITopicClient _topicClient;
+
+        public OrdersController(ITopicClient client)
+        {
+            _topicClient = client;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> SubmitOrder(Order order)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ src/OrderApi/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class Order
+    {
+        [Required]
+        public string AccountId { get; set; }
+
+        [Range(0.01, 1000000)]
+        public decimal PricePerWidget { get; set; }
+
+        [Range(1, 1000)]
+        public int NumberOfWidgets { get; set; }
+    }
+}
