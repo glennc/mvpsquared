1400ede5ff74c80d2dae205fd345bbba698afd6e
Brady Gaster <bradyg@microsoft.com>
2019-03-02 00:32:22 -0800

    repaired build errors

--- src/OrderApi/Startup.cs
+++ src/OrderApi/Startup.cs
@@ -37,7 +37,7 @@ namespace OrderApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
